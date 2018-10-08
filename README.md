2c23a594a97358b00110c11602b0206d087b75ce
youzaiyouzai666 <1620030379@qq.com>
2018-10-08 15:28:11 +0800

    optimization init

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 ## 步骤
 ### 1.使用默认配置
 
-命令行直接`webpack --mode production`，
-  
-  具体参考：[源码中默认设置](https://github.com/webpack/webpack/blob/master/lib/WebpackOptionsDefaulter.js)
+命令行直接` webpack --mode development`，  
+具体参考：[源码中默认设置](https://github.com/webpack/webpack/blob/master/lib/WebpackOptionsDefaulter.js)
+
+
+### 2.splitChunks.chunks= all
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-//index.js
+// //index.js
+// import Vue from 'vue'
+// import(/* webpackChunkName: "asyncModule" */'./a.js')
+//   .then(mod => {
+//     console.log('loaded module a', mod)
+//   })
+
+// console.log('initial module')
+// new Vue({})
+
+
 import Vue from 'vue'
+// import _ from 'lodash'
+
 import(/* webpackChunkName: "asyncModule" */'./a.js')
   .then(mod => {
     console.log('loaded module a', mod)
   })
 
 console.log('initial module')
+// console.log(_.map([1,2,3], a => {
+//     return a * 10
+// }))
 new Vue({})
